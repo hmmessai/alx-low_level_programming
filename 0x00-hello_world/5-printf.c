5bb22f271ac96d9746fe008e6a551fb4b23c3a9e
the <105058702+hmmessai@users.noreply.github.com>
2022-10-13 17:14:20 +0300

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio>
+/**
+ *main - prints the desired output using printf function
+ *
+ *Return: 0 if all goes well, 1 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
