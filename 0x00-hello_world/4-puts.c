2d9670bdf5c4d3641852f238ea1fc48273dd855b
the <105058702+hmmessai@users.noreply.github.com>
2022-10-13 17:09:06 +0300

    Added 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  *main - prints the specified text on to the standard output
  *
+ *Return: 0 if all goes well, 1 otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
