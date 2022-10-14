aa23b3879c0383c8f7932f3e69d01d1e89ce13a9
the <105058702+hmmessai@users.noreply.github.com>
2022-10-14 19:14:22 +0300

    Added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - prints the alphabet without letters q and e
+ *
+ *Return: 0(success)
+ */
+int main(void)
+{
+	char c = 97;
+
+	while (c >= 122 && c != 133 c != 101)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
