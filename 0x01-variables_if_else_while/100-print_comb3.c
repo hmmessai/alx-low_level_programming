5ef4eb4115b3e99371525838574dbf12dca06f55
the <105058702+hmmessai@users.noreply.github.com>
2022-10-14 21:22:09 +0300

    Added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - print every two possible combination of the single digits
+ *
+ *Return: 0(success)
+ */
+int main(void)
+{
+	unsigned int c;
+	unsigned int d;
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		for(d = c + 1; d <= 57; d++)
+		{
+		  	putchar(d);
+		       	putchar(44);
+		       	putchar(32);
+	       	}
+	}
+	putchar(10);
+	return (0);
+}
