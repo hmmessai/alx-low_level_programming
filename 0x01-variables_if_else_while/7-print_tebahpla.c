29b6341278e5e031d005c9b06da01da3f537e3f7
the <105058702+hmmessai@users.noreply.github.com>
2022-10-14 20:05:23 +0300

    Added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.c>
+/**
+ *main - print the lowercase alphabet in reverse order
+ *
+ *Return: 0(Success)
+ */
+int main(void)
+{
+	unsigned char c;
+
+	for (c = 122; c >= 97; c--)
+		putchar(c);
+	putchar(10);
+	return (0);
+}
