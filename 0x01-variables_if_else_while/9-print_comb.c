6755f641b2d7314f66d4f4494aa885c4d1efd6ae
the <105058702+hmmessai@users.noreply.github.com>
2022-10-14 20:29:30 +0300

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - prints comma separated single digit numbers
+ *
+ *Return: 0(success)
+ */
+int main(void)
+{
+	unsigned int c = 48;
+
+	do
+	{
+		putchar(c);
+		putchar(44);
+		putchar(0);
+		c++;
+	} while (c <= 57)
+	putchar(10)
+}
