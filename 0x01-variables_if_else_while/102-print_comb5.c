4bef3da7a617b16e3bd8d833f968c9a5fc0bb862
the <105058702+hmmessai@users.noreply.github.com>
2022-10-15 12:24:25 +0300

    Added 102- print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+/**
+ *main - print all the possible pairs of two digit numbers
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = a + 1; b <= 57; b++)
+		{
+			putchar(a);
+			putchar(b);
+			putchar(32);
+			for (c = 48; c <= 57; c++)
+			{
+				for (d = c + 1; d <= 57; d++)
+				{
+					putchar(c);
+					putchar(d);
+					if (d != 57)
+					{
+						putchar(44);
+					}
+					else
+					{
+						putchar(10);
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
