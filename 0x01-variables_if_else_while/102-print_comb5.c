e9960afdb3dbc4f08589e7549d807cd5037febe4
the <105058702+hmmessai@users.noreply.github.com>
2022-10-15 12:39:50 +0300

    Added correction to 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,17 +15,16 @@ int main(void)
 	{
 		for (b = 48; b <= 57; b++)
 		{
-			putchar(a);
-			putchar(b);
-			putchar(32);
 			for (c = 48; c <= 57; c++)
 			{
 				for (d = 48; d <= 57; d++)
 				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
 					putchar(c);
 					putchar(d);
 					putchar(44);
-					putchar(10);
 				}
 			}
 		}
