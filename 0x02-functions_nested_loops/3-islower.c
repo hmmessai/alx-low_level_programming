461bd2535de62b14b6997d9fac06bc126b970108
the <105058702+hmmessai@users.noreply.github.com>
2022-10-18 10:51:33 +0300

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_islower - idetifies lowercase letters
+ *
+ *Return: 0(if its lowercase) 1(otherwise)
+ */
+int _islower(int c)
+{
+	if (c < 122 && c > 97)
+		return (1);
+	else
+		return (0);
+}
