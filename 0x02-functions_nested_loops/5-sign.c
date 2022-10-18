960b96a37816a90c3ed5cbeb53546b1635c1640c
the <105058702+hmmessai@users.noreply.github.com>
2022-10-18 11:34:59 +0300

    Added 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of the specified number
+ *
+ *Return: 1(if positive), 0(if zero), -1(if negative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
