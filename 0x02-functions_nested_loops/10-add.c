0fce7d6aa6d26c938a3d539c5aca0b715df42fa9
the <105058702+hmmessai@users.noreply.github.com>
2022-10-18 18:23:45 +0300

    Added 10-sum.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - adds two integers
+ *@a: the first addend
+ *@b: the second addend
+ *Return: sum(the sum of the two integers)
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
