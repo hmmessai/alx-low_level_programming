dd7efebea26c072e89ce5a7060d0e1aebe5a52c2
the <105058702+hmmessai@users.noreply.github.com>
2022-10-18 18:09:29 +0300

    Added correction to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@
  */
 void times_table(void)
 {
-	int i, j, product, last_digit, first_digit;
+	int i, j, product;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -16,9 +16,7 @@ void times_table(void)
 			if (product < 10)
 			{
 				if (j == 0)
-				{
 					_putchar(48);
-				}
 				else
 				{
 					_putchar(32);
@@ -26,36 +24,24 @@ void times_table(void)
 					_putchar(product + 48);
 				}
 				if (j != 9)
-				{
 					_putchar(44);
-				}
 				else
-				{
 					_putchar(10);
-				}
 			}
 			else
 			{
 				if (j == 0)
-				{
 					_putchar(48);
-				}
 				else
 				{
 					_putchar(32);
-					last_digit = product % 10;
-					first_digit = product / 10;
-					_putchar(first_digit + 48);
-					_putchar(last_digit + 48);
+					_putchar((product / 10) + 48);
+					_putchar((product % 10) + 48);
 				}
 				if (j != 9)
-				{
 					_putchar(44);
-				}
 				else
-				{
 					_putchar(10);
-				}
 			}
 		}
 	}
