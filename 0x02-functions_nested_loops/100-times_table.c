a2a0d8859308dd1aacf26d3676bda7c47b6409b0
the <105058702+hmmessai@users.noreply.github.com>
2022-10-19 13:41:19 +0300

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,45 +10,39 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if(n < 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				if(j == 0)
-				{
+				if (j == 0)
 					_putchar(48);
-					_putchar(44);
+				else if (i * j > 99)
+				{
+					_putchar(32);
+					_putchar(((i * j) / 100) + 48);
+					_putchar((((i * j) / 10) % 10) + 48);
+					_putchar(((i * j) % 10) + 48);
+				}
+				else if (i * j > 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(((i * j) / 10) + 48);
+					_putchar(((i * j) % 10) + 48);
 				}
 				else
 				{
-					if (i * j > 99)
-					{
-						_putchar(32);
-						_putchar(((i * j) / 100) + 48);
-						_putchar((((i * j) / 10) % 10) + 48);
-						_putchar(((i * j) % 10) + 48);
-					}
-					else if (i * j > 9)
-					{
-						_putchar(32);
-						_putchar(32);
-						_putchar(((i * j) / 10) + 48);
-						_putchar(((i * j) % 10) + 48);
-					}
-					else
-					{
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-						_putchar((i * j) + 48);
-					}
-					if (j == n)
-						_putchar(10);
-					else
-						_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+					_putchar((i * j) + 48);
 				}
+				if (j == n)
+					_putchar(10);
+				else
+					_putchar(44);
 			}
 		}
 	}
