ad0357222f445b422b699a5737f36933882adc1d
the <105058702+hmmessai@users.noreply.github.com>
2022-10-19 15:18:57 +0300

    Added 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - prints the sum of even fibonacci numbers
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned long int i = 1;
+	unsigned long int j = 2;
+	unsigned long int k = 0;
+	unsigned long int sum;
+
+	while (k < 4000000)
+	{
+		k = i + j;
+		if (k % 2 == 0)
+			sum += k;
+		i = j;
+		j = k;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
