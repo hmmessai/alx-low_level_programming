c2091384f60a980631fb8658ba3ec11caf132411
the <105058702+hmmessai@users.noreply.github.com>
2022-10-19 13:12:26 +0300

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,43 +21,16 @@ void print_times_table(int n)
 					_putchar(48);
 					_putchar(44);
 				}
-				else if (j != n)
-				{
-					if (i * j > 100)
-					{
-						_putchar(32);
-						_putchar(((i * j) / 100) + 48);
-						_putchar((((i * j) / 10) % 10) + 48);
-						_putchar(((i * j) % 10) + 48);
-						_putchar(44);
-					}
-					else if (i * j >= 10)
-					{
-						_putchar(32);
-						_putchar(32);
-						_putchar(((i * j) / 10) + 48);
-						_putchar(((i * j) % 10) + 48);
-						_putchar(44);
-					}
-					else
-					{
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-						_putchar((i * j) + 48);
-						_putchar(44);
-					}
-				}
 				else
 				{
-					if (i * j > 100)
+					if (i * j > 99)
 					{
 						_putchar(32);
 						_putchar(((i * j) / 100) + 48);
 						_putchar((((i * j) / 10) % 10) + 48);
 						_putchar(((i * j) % 10) + 48);
 					}
-					else if (i * j > 10)
+					else if (i * j > 9)
 					{
 						_putchar(32);
 						_putchar(32);
@@ -71,9 +44,12 @@ void print_times_table(int n)
 						_putchar(32);
 						_putchar((i * j) + 48);
 					}
+					if (j == n)
+						_putchar(10);
+					else
+						_putchar(44);
 				}
 			}
-			_putchar(10);
 		}
 	}
 }
