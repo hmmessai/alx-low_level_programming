736917667f74927f6f03440f7787eb3a433d7ea5
the <105058702+hmmessai@users.noreply.github.com>
2022-10-19 12:18:41 +0300

    correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,10 @@
 void print_to_98(int n)
 {
 	signed int i, j;
+
 	if (n <= 98)
 	{
-		for(i = n; i < 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
@@ -19,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for(j = n; j > 98; j--)
+		for (j = n; j > 98; j--)
 		{
 			printf("%d, ", j);
 		}
