fb3da81b81bc4c582ec5f3762eb53e3edd066bc7
the <105058702+hmmessai@users.noreply.github.com>
2022-10-20 10:04:55 +0300

    correction

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int print_remaining_days(int, int, int);
+void print_remaining_days(int, int, int);
 int convert_day(int, int);
 int largest_number(int, int, int);
 
