f21c42a44825ed5ec691991186589c179762732b
the <105058702+hmmessai@users.noreply.github.com>
2022-10-20 10:59:13 +0300

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - multiplies two digits
+ *@a: the first number to be multiplied
+ *@b: the second number to be multiplied
+ *Return: product
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
