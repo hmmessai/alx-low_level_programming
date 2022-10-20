07d53010b0de707ae0ff574806f102d8f9f34faa
the <105058702+hmmessai@users.noreply.github.com>
2022-10-20 11:05:23 +0300

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - print all single digits
+ *
+ *Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar(10);
+	return (0);
+}
