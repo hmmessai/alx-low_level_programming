c1f9bc2ab3407a85a0372e273116f310888f263c
the <105058702+hmmessai@users.noreply.github.com>
2022-10-24 13:08:09 +0300

    Added 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,13 +9,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
+		printf("\n");
+	else
 	{
-		if (i != n - 1)
-			printf("%d, ", a[i]);
-		else if(i > 0)
-			printf("%d\n", a[i]);
-		else
-			printf("\n");
+		for (i = 0; i < n; i++)
+		{
+			if (i != n - 1)
+				printf("%d, ", a[i]);
+			else
+				printf("%d\n", a[i]);
+		}
 	}
 }
