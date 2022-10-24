1a405c6670663362894eb3dce3b92f9b1eaf9be0
the <105058702+hmmessai@users.noreply.github.com>
2022-10-24 10:22:36 +0300

    Added 0-reser_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - takes a pointer and set the value it points to 98.
- *@*n: the address to the value to be reset
+ *@n: the address to the value to be reset
  *Return: 0(success)
  */
 void reset_to_98(int *n)
