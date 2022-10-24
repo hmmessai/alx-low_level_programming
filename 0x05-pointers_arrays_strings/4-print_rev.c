b0a7783e5f5b56ee8ef06a85d5d572ecbc6592f1
the <105058702+hmmessai@users.noreply.github.com>
2022-10-24 11:19:12 +0300

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_rev - prints a string in reverse
+ *@s: the string to be reversed
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	while(count >= 0)
+	{
+		_putchar(*(s + count));
+		count--;
+	}
+	_putchar('\n');
+}
