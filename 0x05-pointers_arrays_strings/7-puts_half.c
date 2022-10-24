23c32fe1aea92ceebff062e6998497fc71a32324
the <105058702+hmmessai@users.noreply.github.com>
2022-10-24 12:30:38 +0300

    Added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *puts_half - prints half of the given string
+ *@str: the string in prospect
+ *Return: void
+ */
+void puts_half(char *str)
+{
+	int count = 0;
+	int i;
+
+	while (str[count] != '\0')
+		count++;
+	count--;
+	if (count % 2 == 0)
+		count /= 2;
+	else
+		count = (count - 1) / 2;
+	for (i = 0; i <= count; i++)
+		_putchar(str[count]);
+	_putchar('\n');
+}
