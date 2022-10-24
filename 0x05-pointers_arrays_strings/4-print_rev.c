ad105a8125322b72f127fb556e4c78b289d8beab
the <105058702+hmmessai@users.noreply.github.com>
2022-10-24 11:43:08 +0300

    correction to 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,9 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while (*(s + count) != '\0')
+	while (s[count] != '\0')
 		count++;
 	while (count >= 0)
-		_putchar(*(s + count--));
+		_putchar(s[count]);
 	_putchar('\n');
 }
