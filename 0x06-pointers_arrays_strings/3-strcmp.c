6cb6c6c160ad78715d944e70b6173da7ad8c762d
the <105058702+hmmessai@users.noreply.github.com>
2022-10-26 10:19:35 +0300

    Added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcmp - compares two strings
+ *@s1: the first string
+ *@s2: the second string
+ *Return: +ve if s1 is greater, zero if it matches and -ve if s2 is greater
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int diff = 0;
+	int count = 0;
+
+	while (s1[count] != '\0' && s2[count] != '\0')
+	{
+		if (s1[count] > s2[count])
+			diff++;
+		else if (s1[count] < s2[count])
+			diff--;
+		else
+			diff = diff;
+		count++;
+	}
+	return (diff);
+}
