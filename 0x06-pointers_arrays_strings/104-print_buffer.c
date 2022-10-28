18ce737d130c598112eb21b9e28b98a82673c8a1
the <105058702+hmmessai@users.noreply.github.com>
2022-10-28 08:16:13 +0300

    Added 104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_buffer - prints a buffer
+ *@b: the buffer in prospect
+ *@size: the size of b to be printed
+ *Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	
+}
