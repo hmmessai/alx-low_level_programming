70ccccd49798ce78df316004277008b3e1ebc4a8
the <105058702+hmmessai@users.noreply.github.com>
2022-10-28 09:38:40 +0300

    correction to 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,10 @@ char *cap_string(char *s)
 	{
 		for (j =0;j < 14; j++)
 		{
-			if (s[i] >= 97 && s[i] <= 122)
+			if (s[i] == tchar[j])
 			{
-				if (s[i -1] == tchar[j] || i == 0)
-					s[i] -= 32;
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					s[i + 1] -= 32;
 				break;
 			}
 		}
