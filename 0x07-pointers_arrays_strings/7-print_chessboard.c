757af9a4ccc95ac2a6ad482e30d0ca29207c3fff
the <105058702+hmmessai@users.noreply.github.com>
2022-11-01 09:40:45 +0300

    correction to 5

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_chessboard - prints the chess board
- *
+ *@a: the board to be printed
  *Return: void
  */
 void print_chessboard(char (*a)[8])
@@ -10,7 +10,7 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8;j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
