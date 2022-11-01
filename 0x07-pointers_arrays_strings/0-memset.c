659a25bdd101956280c9e98912d73631e434f1f6
the <105058702+hmmessai@users.noreply.github.com>
2022-11-01 08:34:12 +0300

    correction to 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *memset - fills memory with a constant byte
+ *_memset - fills memory with a constant byte
  *@s: address of the memory to fill
  *@b: the constant byte to fill the memory
  *@n: the number of bytes to fill
@@ -15,5 +15,5 @@ char *_memset(char *s, char b, unsigned int n)
 		*(s + i) = b;
 		i++;
 	}
-	return s;
+	return (s);
 }
