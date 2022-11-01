610682fe42e1be5da78f15e988d0762303dc0b12
the <105058702+hmmessai@users.noreply.github.com>
2022-11-01 09:34:05 +0300

    correction to 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,22 +3,23 @@
  *_strstr - finds the firsr occurence of substring in a given string
  *@haystack: the string in prospect
  *@needle: the substring to be found
- *Return: pointer to the beginning of the located substring
+ *Return: pointer to string at the beginning of the located substring
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
 	while (*haystack)
 	{
+		i = 0;
+
 		if (haystack[i] == needle[i])
 		{
-			while (haystack[i] == needle[i])
-			{
-				if (needle[i + 1] != '\0')
+			do {
+				if (needle[i + 1] == '\0')
 					return (haystack);
 				i++;
-			}
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
