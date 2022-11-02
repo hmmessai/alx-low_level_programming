848b679d7a333e1bbf2633e082a972c3a4eddb0b
the <105058702+hmmessai@users.noreply.github.com>
2022-11-02 19:52:04 +0300

    Added 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,31 @@
 #include "main.h"
 /**
+ *isDivisible - checks if the number is divisible with every possible number
+ *@num: the number in prospect
+ *@div: the dividend
+ *Return: 1 if prime 0 if not
+ */
+int isDivisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+	if (div == num / 2)
+		return (1);
+	return isDivisible(num, div + 1);
+}
+
+/**
  *is_prime_number - checks whether integer is prime or not
  *@n: the integer in prospect
  *Return: 1 if prime 0 if not
  */
 int is_prime_number(int n)
 {
-	int i = 2;
-
-	if ()
-	{
-		i++;
-		
-	}
-}
-int isdivisible(int n, int x)
-{
-	int n;
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n == 3)
+		return (1);
+	return (isDivisible(n, 4));
 }
