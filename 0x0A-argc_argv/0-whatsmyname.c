43ff5d589844e4faa5b8e28c58000403729b4dcf
the <105058702+hmmessai@users.noreply.github.com>
2022-11-05 15:13:33 +0300

    Started this project and added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - outputs the name of the program run to get this output
+ *@argc: the argument count
+ *@argv: the array of the arguments used
+ *Return: the name of the program that run
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+## A-argc_argv
+1.[0-whatsmyname.c](./whatsmyname.c "whatsmyname"): prints the program that run
+2.[]()
