0bbf3603405ff781e4ea20049e3e6e241d30db20
the <105058702+hmmessai@users.noreply.github.com>
2022-11-07 10:53:12 +0300

    correction to 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 	{
-		fprintf(strerr,"Error in memory allocation\n");
+		fprintf(strerr, "Error in memory allocation\n");
 		return (1);
 	}
 	if (size == 0)
@@ -22,5 +22,5 @@ char *create_array(unsigned int size, char c)
 	{
 		s[i] = c;
 	}
-	return s;
+	return (s);
 }
