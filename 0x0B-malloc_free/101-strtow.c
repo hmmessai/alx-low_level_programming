4ccbac05aded9041a58696d800f9a396a879c35d
the <105058702+hmmessai@users.noreply.github.com>
2022-11-07 20:00:40 +0300

    Added 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *strtow - changes string to two words
+ *@str: the string in prospect
+ *Return: returns the pointer to words
+ */
+char **strtow(char *str)
+{
+	char **s;
+
+	for (i = j = 0; str[i] != '\n'; i++)
+	{
+       		for (j = 0; str[j] == ' '; i++, j++)
+       			s[i][j] = str[i];
+	}
+}
