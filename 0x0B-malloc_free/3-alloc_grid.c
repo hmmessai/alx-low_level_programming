1b177bae3bbe5b3e505553613898fa58e8cc7002
the <105058702+hmmessai@users.noreply.github.com>
2022-11-07 13:31:37 +0300

    correction to 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
-			for (i--; i >= 0; i--)
+			for (; i >= 0; i--)
 				free(arr[i]);
 			free(arr);
 		       	return (NULL);
