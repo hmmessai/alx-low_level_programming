1f946957b421a240b39bfb8e4c3aeef80d0dece6
the <105058702+hmmessai@users.noreply.github.com>
2022-11-07 12:20:07 +0300

    Added 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ *str_concat - concatenates two strings
+ *@s1: the first string
+ *@s2: the second string
+ *Return: new string pointer with the concatenated text
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	unsigned int count1 = 0;
+	unsigned int count2 = 0;
+	unsigned int i = 0;
+
+	while (s1[i])
+	{
+		count1++;
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		count2++;
+		i++;
+	}
+	i = 0;
+	s = malloc((count1 + count2) * sizeof(char));
+	if (s == NULL)
+		fprintf(stderr, "Error in memory allocation");
+	while (i < count1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < count2)
+	{
+		s[count1 + i] = s2[i];
+		i++;
+	}
+	return (s);
+}
