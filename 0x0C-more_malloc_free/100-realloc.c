ba20b6084e2737f3f1959ff92f458dc3b54eea44
the <105058702+hmmessai@users.noreply.github.com>
2022-11-11 09:13:31 +0300

    correction to 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,13 +13,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		return (ptr);
 	}
+
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	ptr = malloc(new_size);
+
+	if (ptr == NULL)
+		return (NULL);
+
 	return (ptr);
 }
