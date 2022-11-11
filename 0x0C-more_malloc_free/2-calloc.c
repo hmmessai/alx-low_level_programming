fc35561b877263da95aeca60167bbf67246aae94
the <105058702+hmmessai@users.noreply.github.com>
2022-11-11 08:45:25 +0300

    correction to 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,15 +7,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *arr;
+	char *arr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	arr = malloc(nmemb * size);
+
 	if (arr == NULL)
 		return (NULL);
+
 	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
+
 	return (arr);
 }
