f69cb5fa9dc757788991e199587c3dd59d24c2a7
hmmessai <hmmessai@github.com>
2022-11-17 07:34:40 -0800

    updated header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef __FUNCTION_POINTERS_H__
+#define __FUNCTION_POINTERS_H__
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
@@ -5,3 +7,4 @@ void *print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int array, int size, int (*cmp)(int));
+#endif
