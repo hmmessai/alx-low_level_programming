75772ee82be3a0bd3bb328b14f676844f667d8c8
messai <hmmessai@github.com>
2022-11-19 14:55:49 +0300

    new update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,20 +9,21 @@
 
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		printf("Name: ");
 		if (d ->name == NULL)
-			printf("nil\n");
+			printf("(nil)\n");
 		printf("%s\n". d ->name);
 		printf("Age: ");
 		if (d -> age == NULL)
-			printf("nil\n");
+			printf("(nil)\n");
 		printf("%.6f\n", d -> age);
 		printf("Owner: ");
 		if (d -> owner == NULL)
-			printf("nil\n");
+			printf("(nil)\n");
 		printf("%s\n", d -> owner);
 	}
+	return;
 }
 
