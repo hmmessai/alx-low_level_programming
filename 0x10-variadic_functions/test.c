aa415db70957b44fc2a76b81c81b43fffbdebc76
messai <hmmessai@github.com>
2022-11-20 11:21:23 +0300

    cirrection to 1

--- 0x10-variadic_functions/test.c
+++ 0x10-variadic_functions/test.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-  int sum;
-
-  sum = sum_them_all(2, 98,1024);
-  printf("%d\n", sum);
-  return (0);
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
 }
