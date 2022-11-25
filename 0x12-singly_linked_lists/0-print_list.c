3643289798c7a876b29a6ab370ab1558ef99ddd3
messai <hmmessai@github.com>
2022-11-25 22:18:54 +0300

    correction to 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,14 +16,15 @@ size_t print_list(const list_t *h)
 	if (h == NULL)
 		return (0);
 
-
-	if (h->next == NULL)
-		return (1);
-
 	if (h->str != NULL)
 		printf("[%d] %s\n", h->len, h->str);
 	else
 		printf("[0] (nil)\n");
+
+
+	if (h->next == NULL)
+                return (1);
+
 	a = h->next;
 	nodeTotal = 1;
 
