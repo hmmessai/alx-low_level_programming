736eee66e2943ca5f83d6ef8d204d8955605f66e
messai <hmmessai@github.com>
2022-11-26 09:53:16 +0300

    added 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /*
- * before_main - prints something before the main function is executed 
- *
- * Return: void
+ *before_main - prints something before the main function is executed 
+ *Return: void
  */
 
 void __attribute__((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
