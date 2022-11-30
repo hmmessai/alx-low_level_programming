818b45ca2bd02bcfd593934019347814960d7000
messai <hmmessai@github.com>
2022-11-30 18:06:41 +0300

    correction to 6;

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,6 +12,7 @@ int pop_listint(listint_t **head)
 {
 	listint_t *h;
 	listint_t *curr;
+	int num;
 
 	curr = *head;
 
@@ -19,11 +20,12 @@ int pop_listint(listint_t **head)
 		return (0);
 
 	h = curr->next;
+	num = curr->n;
 
 	free(curr);
 
 	*head = h;
 
-	return (h->n);
+	return (num);
 
 }
