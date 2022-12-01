728cf8a6eae5a2935bb875f752f1d4f40a330e4f
messai <hmmessai@gihub.com>
2022-12-01 11:15:46 +0300

    Added 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of a bit at a given index
+ *
+ * @n: the number to be evaluated
+ * @index: the index of the bit to get
+ *
+ * Return: value of the bit at index
+ * 	-1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; i++)
+	{
+		if (i == index)
+			return(n & 1);
+		n >>= 1;
+	}
+	
+	return (-1);
+
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 
 int power(int x, int y);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
