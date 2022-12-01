26ebc9de5dc9d99c988125bbd750e21c2916f30a
messai <hmmessai@gihub.com>
2022-12-01 09:08:14 +0300

    Started a new project and added some files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary number to an unsigned int
+ *
+ * @b: pointer to a string of 1's and 0's
+ *
+ * Return: the converted number
+ * 	zero if b contains non binary characters or is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i;
+	int len = 0, bnum;
+	int n = 0;
+
+	if (b != NULL)
+	{
+		for (i = 0; b[i]; i++)
+			len++;
+
+		for (i = len; i < 0; i--)
+		{
+			if (b[i] != '0' || b[i] != '1')
+				return (0);
+			if (b[len] == 48)
+				bnum = 0;
+			if (b[len] == 49)
+				bnum = 1;
+
+			num += power(2, n) * bnum;
+			n++;
+		}
+
+		return (num);
+	}
+	return (0);
+}
+
+#include "main.h"
+/**
+ *power - computes the value of x raised to y
+ *@x: the number to be multiplied y times
+ *@y: the number of times x will be multiplied
+ *Return: the value of x raised to y
+ */
+int power(int x, int y)
+{
+	int res;
+
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+	{
+		return (1);
+	}
+	y--;
+	res = x * power(x, y);
+	return (res);
+}
