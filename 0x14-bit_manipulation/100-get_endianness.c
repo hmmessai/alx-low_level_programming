eb890e07036b2229552743a7d713f285c4a681cf
messai <hmmessai@gihub.com>
2022-12-02 08:44:18 +0300

    Added 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian
+ *	1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int i;
+	char *c = (char *)&i;
+
+	if (c)
+		return (1);
+	else
+		return(0);
+}
