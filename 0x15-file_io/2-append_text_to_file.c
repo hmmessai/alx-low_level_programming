dbd9394e48c79929a2c12081de7a3591c7764f35
messai <hmmessai@github.com>
2022-12-05 16:16:26 +0300

    Added 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (letters = 0; text_content[letters], letters++)
+		for (letters = 0; text_content[letters]; letters++)
 			;
 
 		bytes_wrote = write(fd, text_content, letters);
