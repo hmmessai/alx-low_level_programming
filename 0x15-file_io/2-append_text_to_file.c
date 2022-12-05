ea34116dcddc4f01b3ed29028cc5d691f554e087
messai <hmmessai@github.com>
2022-12-05 16:03:53 +0300

    Added 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	int letters;
 	int bytes_wrote;
 
+
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
