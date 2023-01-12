ce9099fdc3304ee0d7b1bc051205406f3f7680cd
Messai <messai@github.com>
2023-01-12 21:00:47 +0300

    Added 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets the nth node of a list
+ * @head: the head of the list
+ * @index: the index of the node we want
+ * Return: the nth node of the list
+ *	NULL if it failed
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+}
