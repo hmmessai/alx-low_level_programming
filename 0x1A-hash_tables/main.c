d127af5d49bb27eb4747f9d5118bba3f69f48c38
Messai <messai@github.com>
2023-02-03 05:54:18 +0300

    correction

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -4,15 +4,32 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
+ * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    hash_table_t *ht;
+    shash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
+    ht = shash_table_create(1024);
+    shash_table_set(ht, "y", "0");
+    shash_table_print(ht);
+    shash_table_set(ht, "j", "1");
+    shash_table_print(ht);
+    shash_table_set(ht, "c", "2");
+    shash_table_print(ht);
+    shash_table_set(ht, "b", "3");
+    shash_table_print(ht);
+    shash_table_set(ht, "z", "4");
+    shash_table_print(ht);
+    shash_table_set(ht, "n", "5");
+    shash_table_print(ht);
+    shash_table_set(ht, "a", "6");
+    shash_table_print(ht);
+    shash_table_set(ht, "m", "7");
+    shash_table_print(ht);
+    shash_table_print_rev(ht);
+        shash_table_delete(ht);
     return (EXIT_SUCCESS);
 }
